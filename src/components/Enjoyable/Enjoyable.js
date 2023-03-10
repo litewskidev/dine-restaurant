dbfa8dbeb0b64dc0b28c9fe359f126d796b8855d
Adrian Litewski <litewskidev@gmail.com>
2023-03-11 00:49:01 +0100

    start modify Enjoyable component

--- src/components/Enjoyable/Enjoyable.js
+++ src/components/Enjoyable/Enjoyable.js
@@ -2,9 +2,11 @@ import styles from './Enjoyable.module.scss';
 
 const Enjoyable = () => {
   return(
-    <div>
-      <h2>Enjoyable place for all the family</h2>
-      <p>Our relaxed surroundings make dining with us a great experience for everyone. We can even arrange a tour of the farm before your meal.</p>
+    <div className={styles.enjoy}>
+      <div className={styles.enjoy__description}>
+        <h2 className={styles.enjoy__h2}>Enjoyable place<br />for all the family</h2>
+        <p className={styles.enjoy__p}>Our relaxed surroundings make dining with us a great experience for everyone. We can even arrange a tour of the farm before your meal.</p>
+      </div>
     </div>
   );
 };

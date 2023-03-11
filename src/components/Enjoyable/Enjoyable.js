23789a2934e30265fe6987d2184cd1c709e330e9
Adrian Litewski <litewskidev@gmail.com>
2023-03-11 20:41:07 +0100

    add Enjoyable component & new typography

--- src/components/Enjoyable/Enjoyable.js
+++ src/components/Enjoyable/Enjoyable.js
@@ -1,11 +1,26 @@
 import styles from './Enjoyable.module.scss';
+import pattern from "../../images/patterns/pattern-divide.svg";
+import React from "react";
 
 const Enjoyable = () => {
   return(
     <div className={styles.enjoy}>
-      <div className={styles.enjoy__description}>
-        <h2 className={styles.enjoy__h2}>Enjoyable place<br />for all the family</h2>
-        <p className={styles.enjoy__p}>Our relaxed surroundings make dining with us a great experience for everyone. We can even arrange a tour of the farm before your meal.</p>
+      <div className={styles.enjoy__content}>
+        <div className={styles.enjoy__img}></div>
+        <div className={styles.enjoy__description}>
+          <img className={styles.pattern} src={pattern} alt="" />
+          <div>
+            <h2 className="text__L">
+              Enjoyable place <br />
+              for all the family
+            </h2>
+            <p className="p__text">
+              Our relaxed surroundings make dining with us a great experience
+              for everyone. We can even arrange a tour of the farm before your
+              meal.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/HeroHome/HeroHome.js
+++ src/components/HeroHome/HeroHome.js
@@ -8,8 +8,14 @@ const HeroHome = () => {
         <h2>dine</h2>
       </div>
       <div className={styles.header__description}>
-        <h1 className={styles.header__h1}>Exquisite dining<br/>since 1989</h1>
-        <p className={styles.header__p}>Experience our seasonal menu in beautiful country surroundings. Eat the freshest produce from the comfort of our farmhouse.</p>
+        <h1 className={styles.header__h1}>
+          Exquisite dining<br/>since 1989
+        </h1>
+        <p className={styles.header__p}>
+          Experience our seasonal menu in beautiful country
+          surroundings. Eat the freshest produce from the comfort
+          of our farmhouse.
+        </p>
       </div>
       <ButtonBook>Book a table</ButtonBook>
     </header>

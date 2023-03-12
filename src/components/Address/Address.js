7a7f79f44460df9f6d52086e63cd8684ecc00fd1
Adrian Litewski <litewskidev@gmail.com>
2023-03-12 23:01:15 +0100

    add Adress component

--- src/components/Address/Address.js
+++ src/components/Address/Address.js
@@ -3,18 +3,20 @@ import styles from './Address.module.scss';
 const Address = () => {
   return(
     <div className={styles.address}>
-      <div>
+      <div className={styles.address__content}>
         <h2>dine</h2>
-      </div>
-      <div>
-        <p>Marthwaite, Sedbergh</p>
-        <p>Cumbria</p>
-        <p>+00 44 123 4567</p>
-      </div>
-      <div>
-        <p>Open Times</p>
-        <p>Mon - Fri: 09:00 AM - 10:00 PM</p>
-        <p>Sat - Sun: 09:00 AM - 11:30 PM</p>
+        <div className={styles.address__contact}>
+          <div>
+            <p>Marthwaite, Sedbergh</p>
+            <p>Cumbria</p>
+            <p>+00 44 123 4567</p>
+          </div>
+          <div>
+            <p>Open Times</p>
+            <p>Mon - Fri: 09:00 AM - 10:00 PM</p>
+            <p>Sat - Sun: 09:00 AM - 11:30 PM</p>
+          </div>
+        </div>
       </div>
     </div>
   );

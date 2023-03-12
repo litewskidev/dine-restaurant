e482a485a722e13e98350e5c131f0cb1ae22b419
Adrian Litewski <litewskidev@gmail.com>
2023-03-12 23:50:38 +0100

    add  HeroBooking module

--- src/components/Booking/Booking.js
+++ src/components/Booking/Booking.js
@@ -1,12 +1,19 @@
 import Address from "../Address/Address";
 import BookingForm from "../BookingForm/BookingForm";
 import HeroBooking from "../HeroBooking/HeroBooking";
+import styles from './Booking.module.scss';
+import pattern__lines from '../../images/patterns/pattern-lines.svg';
 
 const Booking = () => {
   return(
     <main>
-      <HeroBooking />
-      <BookingForm />
+      <div className={styles.booking}>
+        <HeroBooking />
+        <BookingForm />
+      </div>
+      <div className={styles.booking__img}>
+        <img className={styles.pattern__lines} src={pattern__lines} alt='' />
+      </div>
       <Address />
     </main>
   );
--- src/components/HeroBooking/HeroBooking.js
+++ src/components/HeroBooking/HeroBooking.js
@@ -2,10 +2,14 @@ import styles from './HeroBooking.module.scss';
 
 const HeroBooking = () => {
   return(
-    <div>
-      <h2>dine</h2>
-      <h1>Reservations</h1>
-      <p>We can’t wait to host you. If you have any special requirements please feel free to call on the phone number below. We’ll be happy to accommodate you.</p>
+    <div className={styles.herobooking}>
+      <h2 className={styles.herobooking__title}>dine</h2>
+      <div className={styles.herobooking__description}>
+        <h1 className='text__L'>Reservations</h1>
+        <p className='p__text'>We can’t wait to host you.
+        If you have any special requirements please feel free
+        to call on the phone number below. We’ll be happy to accommodate you.</p>
+      </div>
     </div>
   );
 };

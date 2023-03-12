dc430970ebd0fb466348b8722c420efb61f0e314
Adrian Litewski <litewskidev@gmail.com>
2023-03-12 21:54:18 +0100

    add Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,10 +4,12 @@ import styles from './Footer.module.scss';
 
 const Footer = () => {
   return(
-    <div>
-      <h2>Ready to make a reservation?</h2>
-      <ButtonBook>Book a table</ButtonBook>
-      <Address />
+    <div className={styles.footer}>
+      <div className={styles.footer__content}>
+          <h2 className='text__L'>Ready to make a reservation?</h2>
+          <ButtonBook class='white'>Book a table</ButtonBook>
+      </div>
+        <Address />
     </div>
   );
 };

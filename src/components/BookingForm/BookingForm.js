dd53560f634646ac9ffccc52c0e3f959e5dff8e7
Adrian Litewski <litewskidev@gmail.com>
2023-03-14 00:15:30 +0100

    fix submit form

--- src/components/BookingForm/BookingForm.js
+++ src/components/BookingForm/BookingForm.js
@@ -6,25 +6,26 @@ const BookingForm = () => {
     <div className={styles.bookingform}>
       <form>
         <div className={styles.bookingform__text}>
-          <input type='text' placeholder='Name' />
-          <input type='email' placeholder='Email'/>
+          <input type='text' placeholder='Name' required />
+          <input type='email' placeholder='Email' required />
         </div>
         <div className={styles.bookingform__numbers}>
           <p>Pick a date</p>
-          <input type="number" placeholder='MM' />
-          <input type="number" placeholder='DD' />
-          <input type="number" placeholder='YYYY' />
+          <input type="number" placeholder='MM' required />
+          <input type="number" placeholder='DD' required />
+          <input type="number" placeholder='YYYY' required />
           <p>Pick a time</p>
-          <input type="number" placeholder='09' />
-          <input type="number" placeholder='00' />
+          <input type="number" placeholder='09' min={9} required />
+          <input type="number" placeholder='00' min={0} required />
           <select>
             <option>AM</option>
             <option>PM</option>
           </select>
         </div>
+        <NumInput />
+        <input type='submit' value='Make Reservation' className={styles.black} />
       </form>
-      <NumInput />
-      <button className={styles.black}>Make Reservation</button>
+
     </div>
   );
 };

91751e9efadffea180d6fd026a4812c2060a958b
Adrian Litewski <litewskidev@gmail.com>
2023-03-13 23:53:45 +0100

    add NumInput logic

--- src/utils/NumInput/NumInput.js
+++ src/utils/NumInput/NumInput.js
@@ -1,14 +1,33 @@
 import { useState } from 'react';
 import styles from './NumInput.module.scss';
+import plus from '../../images/icons/icon-plus.svg';
+import minus from '../../images/icons/icon-minus.svg';
 
 const NumInput = () => {
 
   const [peopleNum, setPeopleNum] = useState(4);
 
+  const increment = () => {
+    if(peopleNum <= 9) {
+    setPeopleNum(peopleNum + 1);
+    }
+  };
+
+  const decrement = () => {
+    if(peopleNum >= 2) {
+    setPeopleNum(peopleNum - 1);
+    }
+  };
+
 
   return(
     <div className={styles.numinput}>
-      <h4>{peopleNum} people</h4>
+      <div className={styles.numinput__content}>
+        <img src={minus} onClick={decrement} alt=''/>
+        <h4>{peopleNum} people</h4>
+        <img src={plus} onClick={increment} alt='' />
+      </div>
+      <div className={styles.numinput__line}></div>
     </div>
   );
 };

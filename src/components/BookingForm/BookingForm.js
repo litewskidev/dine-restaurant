1fc8069f216797604bbb33723754cdc7bf2bc923
Adrian Litewski <litewskidev@gmail.com>
2023-03-14 22:07:21 +0100

    add BookingForm logic

--- src/components/BookingForm/BookingForm.js
+++ src/components/BookingForm/BookingForm.js
@@ -1,28 +1,131 @@
+import { useState } from 'react';
 import NumInput from '../NumInput/NumInput';
 import styles from './BookingForm.module.scss';
 
 const BookingForm = () => {
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [month, setMonth] = useState('');
+  const [day, setDay] = useState('');
+  const [year, setYear] = useState('');
+  const [hour, setHour] = useState('');
+  const [minute, setMinute] = useState('');
+  const [meridiem, setMeridiem] = useState('AM');
+  const [peopleNum, setPeopleNum] = useState(4);
+
+  const minuteFormat = (minute < 10) ? minute + '0' : minute;
+
+  const summary = {
+    Name: name,
+    Email: email,
+    Date: month + '.' + day + '.' + year,
+    Time: hour + ':' + minuteFormat + ' ' + meridiem,
+    People: peopleNum
+  };
+
+  const increment = () => {
+    if(peopleNum <= 9) {
+    setPeopleNum(peopleNum + 1);
+    }
+  };
+
+  const decrement = () => {
+    if(peopleNum >= 2) {
+    setPeopleNum(peopleNum - 1);
+    }
+  };
+
+  const reservation = (e) => {
+    e.preventDefault();
+    console.log(summary);
+
+    setName('');
+    setEmail('');
+    setMonth('');
+    setDay('');
+    setYear('');
+    setHour('');
+    setMinute('');
+    setMeridiem('');
+    setPeopleNum(4);
+  };
+
   return(
     <div className={styles.bookingform}>
-      <form>
+      <form onSubmit={reservation}>
         <div className={styles.bookingform__text}>
-          <input type='text' placeholder='Name' required />
-          <input type='email' placeholder='Email' required />
+          <input
+            onChange={e => setName(e.target.value)}
+            id='name'
+            type='text'
+            placeholder='Name'
+            value={name}
+            required />
+          <input
+            onChange={e => setEmail(e.target.value)}
+            id='email'
+            type='email'
+            placeholder='Email'
+            value={email}
+            required />
         </div>
         <div className={styles.bookingform__numbers}>
           <p>Pick a date</p>
-          <input type="number" placeholder='MM' required />
-          <input type="number" placeholder='DD' required />
-          <input type="number" placeholder='YYYY' required />
+          <input
+            onChange={e => setMonth(e.target.value)}
+            id='month'
+            type="number"
+            placeholder='MM'
+            value={month}
+            min={1}
+            max={12}
+            required />
+          <input
+            onChange={e => setDay(e.target.value)}
+            id='day'
+            type="number"
+            placeholder='DD'
+            value={day}
+            min={1}
+            max={31}
+            required />
+          <input
+            onChange={e => setYear(e.target.value)}
+            id='year'
+            type="number"
+            placeholder='YYYY'
+            value={year}
+            min={2023}
+            required />
           <p>Pick a time</p>
-          <input type="number" placeholder='09' min={9} required />
-          <input type="number" placeholder='00' min={0} required />
-          <select>
-            <option>AM</option>
-            <option>PM</option>
+          <input
+            onChange={e => setHour(e.target.value)}
+            id='hour'
+            type="number"
+            placeholder='09'
+            value={hour}
+            min={1}
+            max={12}
+            required />
+          <input
+            onChange={e => setMinute(e.target.value)}
+            id='minute'
+            type="number"
+            placeholder='00'
+            value={minute}
+            min={0}
+            max={59}
+            required />
+          <select
+            onChange={e => setMeridiem(e.target.value)}
+            id='meridiem'
+            value={meridiem}>
+              <option>AM</option>
+              <option>PM</option>
           </select>
         </div>
-        <NumInput />
+        <NumInput increment={increment} decrement={decrement} people={peopleNum} />
         <input type='submit' value='Make Reservation' className={styles.black} />
       </form>
     </div>
--- src/components/NumInput/NumInput.js
+++ src/components/NumInput/NumInput.js
@@ -3,29 +3,17 @@ import styles from './NumInput.module.scss';
 import plus from '../../images/icons/icon-plus.svg';
 import minus from '../../images/icons/icon-minus.svg';
 
-const NumInput = () => {
+const NumInput = (props) => {
 
-  const [peopleNum, setPeopleNum] = useState(4);
 
-  const increment = () => {
-    if(peopleNum <= 9) {
-    setPeopleNum(peopleNum + 1);
-    }
-  };
-
-  const decrement = () => {
-    if(peopleNum >= 2) {
-    setPeopleNum(peopleNum - 1);
-    }
-  };
 
 
   return(
     <div className={styles.numinput}>
       <div className={styles.numinput__content}>
-        <img src={minus} onClick={decrement} alt=''/>
-        <h4>{peopleNum} people</h4>
-        <img src={plus} onClick={increment} alt='' />
+        <img src={minus} onClick={props.decrement} alt=''/>
+        <h4>{props.people} people</h4>
+        <img src={plus} onClick={props.increment} alt='' />
       </div>
       <div className={styles.numinput__line}></div>
     </div>
